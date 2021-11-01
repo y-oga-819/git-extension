66d8e4e878baa1e999d901bf4e2023e9a9d7aa23
y-oga <y-oga@binc.jp>
2021-11-01 09:54:39 +0900

    現在のブランチ名を取得時、HEADをつけないと正しく取得できなかったので修正、末尾の改行をtrim

--- main.go
+++ main.go
@@ -21,10 +21,10 @@ func main() {
 	// masterとdevelopmentブランチに直接commitするのは禁止
 	if args[0] == "commit" {
 		// 現在のブランチを取得
-		result, _ := exec.Command("git", []string{"rev-parse", "--abbrev-ref"}...).Output()
-		branchName := string(result)
+		result, _ := exec.Command("git", []string{"rev-parse", "--abbrev-ref", "HEAD"}...).Output()
+		branchName := strings.TrimRight(string(result), "\n")
 		if branchName == "master" || branchName == "development" {
-			fmt.Print("\n\n[ERROR] DON'T MERGE INTO THE MASTER BRANCH FOR ANY REASON!\n\n\n")
+			fmt.Print("\n\n[ERROR] DON'T MERGE INTO THE MASTER OR DEVELOPMENT BRANCH FOR ANY REASON!\n\n\n")
 			os.Exit(0)
 		}
 	}
