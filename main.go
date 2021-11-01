6e58e0a68323b239bdca6dd289b956f55e4e35bc
y-oga <y-oga@binc.jp>
2021-11-01 09:41:30 +0900

    flagパッケージのせいでgit側のオプションを網羅していないとエラー吐いたのでos.Args使う

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -9,18 +8,18 @@ import (
 )
 
 func main() {
-	flag.Parse()
+	args := os.Args[1:]
 
 	// masterと名前のつくブランチを対象にしたマージ操作は禁止
-	if flag.Arg(0) == "merge" {
-		if -1 != strings.Index(flag.Arg(1), "master") {
+	if args[0] == "merge" {
+		if -1 != strings.Index(args[1], "master") {
 			fmt.Print("\n\n[ERROR] DON'T MERGE INTO THE MASTER BRANCH FOR ANY REASON!\n\n\n")
 			os.Exit(0)
 		}
 	}
 
 	// masterとdevelopmentブランチに直接commitするのは禁止
-	if flag.Arg(0) == "commit" {
+	if args[0] == "commit" {
 		// 現在のブランチを取得
 		result, _ := exec.Command("git", []string{"rev-parse", "--abbrev-ref"}...).Output()
 		branchName := string(result)
@@ -31,7 +30,7 @@ func main() {
 	}
 
 	// gitコマンドを生成
-	cmd := exec.Command("git", flag.Args()...)
+	cmd := exec.Command("git", args...)
 
 	// 色情報を保持するために標準出力と標準エラー出力を置換
 	cmd.Stdout = os.Stdout
